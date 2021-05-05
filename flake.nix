171a125b158744d95e756f7242795016a31ba820
felix-lipski <felix.lipski7@gmail.com>
2021-05-05 10:54:42 +0000

    add utils, update flake output

--- flake.nix
+++ flake.nix
@@ -3,9 +3,10 @@
 
   outputs = { self, nixpkgs }: {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    spacelix = import ./spacelix.nix;
 
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+  # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+  # defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
 
   };
 }
