7952e2096274319417c1e9ce84fae2f0f3f96c7a
felix-lipski <felix.lipski7@gmail.com>
2021-05-07 19:33:13 +0000

    add pywal generation

--- Makefile
+++ Makefile
@@ -2,5 +2,9 @@ export:
 	nix build .#export
 xresources:
 	cp -r result/xresources xresources
+pywal:
+	cp -r result/pywal pywal
+
 clean:
 	sudo rm -drf xresources
+	sudo rm -drf pywal
--- export.nix
+++ export.nix
@@ -3,9 +3,15 @@ let
   lib           = nixpkgs.lib;
   utils         = (import ./utils.nix) {inherit lib;};
   spacelix      = (import ./spacelix.nix) {inherit lib;};
+  
   xresTempStr   = builtins.readFile ./templates/xresources;
   xresTempPath  = builtins.toFile "xresources" xresTempStr;
+  pywalTempStr  = builtins.readFile ./templates/pywal;
+  pywalTempPath = builtins.toFile "pywal" pywalTempStr;
+
   withGray      = shades: shades.dark // {grey = shades.light.black;};
+  palPath       = tempStr: palette: builtins.toFile "foo" 
+    (utils.interpolateColors (withGray palette) tempStr);
   xresPalPath   = palette: builtins.toFile "foo" 
     (utils.interpolateColors (withGray palette) xresTempStr);
 in
@@ -13,15 +19,21 @@ pkgs.pkgs.stdenv.mkDerivation rec {
   pname       = "spacelix";
   version     = "0.0.1";
   dontUnpack  = true;
-  buildPhase  = utils.concatLines
-    (map (x: "cat ${xresPalPath spacelix."${x}"} > spacelix-${x}-xresources") spacelix.names);
-# buildPhase  = ''
-#   cat ${xresPalPath spacelix.slate} > txt
-# '';
-  installPhase = "mkdir -p $out/xresources\n" + (utils.concatLines
+  buildPhase  = 
+  let
+    genXres = utils.concatLines
+    (map (x: "cat ${palPath xresTempStr spacelix."${x}"} > spacelix-${x}-xresources") spacelix.names);
+    genPywal = utils.concatLines
+    (map (x: "cat ${palPath pywalTempStr spacelix."${x}"} > spacelix-${x}-pywal.json") spacelix.names);
+  in
+  genXres + genPywal;
+
+  installPhase = 
+  let
+    exportXres = "mkdir -p $out/xresources\n" + (utils.concatLines
     (map (x: "mv spacelix-${x}-xresources $out/xresources") spacelix.names));
-# installPhase = ''
-#   mkdir -p $out/bin
-#   mv txt $out/bin
-# '';
+    exportPywal = "mkdir -p $out/pywal\n" + (utils.concatLines
+    (map (x: "mv spacelix-${x}-pywal.json $out/pywal") spacelix.names));
+  in
+  exportXres + exportPywal;
 }
